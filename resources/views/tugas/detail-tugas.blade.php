75e2e762377ff452738b9693f321839de929269a
Fakhri Azmi Husaini <rifakhrii@gmail.com>
2025-06-25 09:33:43 +0700

    modal edit

---
+++ resources/views/tugas/detail-tugas.blade.php
@@ -0,0 +1,30 @@
+<div class="modal fade" id="taskmodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Tambah Tugas</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="judul" class="form-label">Judul</label>
+                    <input type="text" class="form-control" id="judul">
+                </div>
+                <div class="mb-3">
+                    <label for="deskripsi" class="form-label">Deskripsi</label>
+                    <textarea rows="3" class="form-control" id="deskripsi"></textarea>
+                </div>
+                <div class="row">
+                    <div class="col-8 text-start">
+                        <label for="list" class="form-label">Tambah List</label>
+                        <input type="text" class="form-control" id="list" placeholder="Tambah List">
+                    </div>
+                </div>
+                <div class="text-end">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Tambah</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/tugas/index.blade.php
+++ resources/views/tugas/index.blade.php
@@ -3,12 +3,12 @@
 @push('css')
     <style>
         body {
-            background-color: greenyellow;
+            background-color: burlywood;
         }
 
         .card:hover {
             cursor: pointer;
-            background-color: yellowgreen
+            background-color: rgb(180, 231, 255)
         }
 
         .done {
@@ -25,20 +25,21 @@
         <input type="text" class="form-control" id="key" placeholder="Cari Tugas" aria-label="Cari Tugas"
             aria-describedby="button-addon2">
         <button class="btn btn-success" type="button" id="button-tambah" data-bs-toggle="modal"
-            data-bs-target="#exampleModal">Cari</button>
+            data-bs-target="#exampleModal">Tambah</button>
     </div>
-    @include('tugas.modal')
+    @includeIf('tugas.modal')
+    @includeIf('tugas.detail-tugas')
     <div class="list" id="container-list">
         <div class="card">
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-6">
-                        <h6>Tugas</h6>
+                        <h6 class="fw-bold fs-3">Tugas</h6>
                     </div>
                     <div class="col-md-6 text-end">
                         <div class="btn-group" role="group" aria-label="Basic example">
                             <button type="button" class="btn btn-success btn-sm"><i class="fa-solid fa-check"></i></button>
-                            <button type="button" class="btn btn-warning btn-sm"><i
+                            <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal" data-bs-target="#taskmodal"><i
                                     class="fa-solid fa-pen-to-square"></i></button>
                             <button type="button" class="btn btn-danger btn-sm"><i class="fa-solid fa-trash"></i></button>
                         </div>
